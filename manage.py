b050c677689ed8b9f762564d089231fd5e9aa765
Risper Akinyi <email@example.com>
2019-08-19 18:01:16 +0300

    add decorater manager.shell

--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
 from app import create_app,db
 from flask_script import Manager,Server
 from flask_migrate import Migrate,MigrateCommand
-from app.models import User,Comment,Role,DriftPost
+from app.models import User,Session,SessionType
 
 app = create_app('development')
 
@@ -10,8 +10,11 @@ manager.add_command('server',Server)
 
 manager=Manager(app,db)
 manager.add_command('db',MigrateCommand)
+
 @manager.shell
 def make_shell_context():
-    return dict (app=app,db=db,User=User,)
+    return dict (app=app,db=db,User=User,Session=Session,SessionType=SessionType)
+
+    
 if __name__=='__main__':
     manager.run()
