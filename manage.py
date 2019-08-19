89befb815845966f6a8ab55c5c7f63976d35adee
Risper Akinyi <email@example.com>
2019-08-19 18:42:54 +0300

    add changes to auth/form.py

--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@ from app import create_app,db
 from flask_script import Manager,Server
 from flask_migrate import Migrate,MigrateCommand
 from app.models import User,Session,SessionType
-#
+
 app = create_app('development')
 
 manager=Manager(app)
