44edcd6f46ad5a63c26fb3c91ce8fb2e131d58d6
Risper Akinyi <email@example.com>
2019-08-19 17:48:07 +0300

    fix errors

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,10 @@ from flask import Flask
 from flask_bootstrap import Bootstrap
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
  
 db = SQLAlchemy()
-login_manager = LoginManger()
+login_manager = LoginManager()
 login_manager.session_protection = "strong"
 login_manager.login_view = "auth.login"
 Bootstrap=Bootstrap()
@@ -13,7 +14,8 @@ def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config_options[confg_name])
 
-
     db.init_app(app)
     login_manager.init_app(app)
     bootstrap.init_app(app)
+
+    return app
