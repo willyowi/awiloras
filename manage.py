f66314ef17c4d9c654c363880a08d1ec051ed17e
Risper Akinyi <email@example.com>
2019-08-19 17:28:40 +0300

    add session type to  manage.py

--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
 from app import create_app,db
 from flask_script import manager,Server
 from flask_migrate import Migrate,MigrateCommand
-from app.models import User,Session
+from app.models import User,Session,SessionType
 
 app = create_app('development')
 
@@ -12,6 +12,6 @@ manager=Manager(app,db)
 manager.add_command('db',MigrateCommand)
 @manager.shell
 def make_shell_context():
-    return dict (app=app,db=db,User=User,)
+    return dict (app=app,db=db,User=User,Session=Session,SessionType=SessionType)
 if __name__=='__main__':
     manager.run()
