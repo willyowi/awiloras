bc483e56826126581dd0a76e5e0a6806c0abf936
willyowi <wilsonowino1@gmail.com>
2019-08-19 11:09:36 +0300

    auth validate login

--- app/auth/views.py
+++ app/auth/views.py
@@ -11,4 +11,14 @@ def login()
     form=LoginForm()
 
 
-    
+
+    if form.validate_on_submit():
+        user=User.query.filter_by(email=form.email.data).first()
+
+        if user is not None and user.verify_password(form.password.data):
+            
+            login_user(user,form.remember.data)
+
+            return redirect(request.args.get('next') or url_for('main.home'))
+
+        flash('Invalid Username or Password','danger')
