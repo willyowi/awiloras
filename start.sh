fa945afbae4d29f6c4723a3bccceea61720c3f5d
Risper Akinyi <email@example.com>
2019-08-19 17:20:37 +0300

    add start.sh

---
+++ start.sh
@@ -0,0 +1,7 @@
+export SECRET_KEY='owino'
+export DATABASE_URL='postgresql+psycopg2://moringa:risperhub3@localhost/promo'
+export MAIL_USERNAME='risperakinyi3@gmail.com'
+export MAIL_PASSWORD='risperhub3'
+
+
+python3.6 manage.py server
