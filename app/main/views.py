1545b08f006ccbafc6374c3a345d1751274efd4e
Risper Akinyi <email@example.com>
2019-08-19 22:19:49 +0300

    setup index view function

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,16 @@
-from flask import render_template,redirect,url_for,abort,flash,request
-from .. import db
+from flask import render_template,request,redirect,url_for,abort,flash,request
 from . import main
 from flask_login import login_required,current_user
-from ..models import User
-from .forms import Session_typesForm,SessionForm
-from ..image_upload import add_profile_pic,add_drift_image
+from ..models import User,Session
+from ..import db
+
+
+# Views
+@main.route('/')
+def index():
+
+  '''
+  View root page function that returns the index page and its data
+  '''
+  
+  return render_template('index.html',user=current_user)
