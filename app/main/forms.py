53c2ff43183a922625d18f9e103c2af08b79238c
willyowi <wilsonowino1@gmail.com>
2019-08-20 10:50:17 +0300

    profile pic upload

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,6 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,TextAreaField,SubmitField,IntegerField,SelectField
-from wtforms.fields.html5 import DateField
+from wtforms import StringField,SubmitField,TextAreaField
+from wtforms.validators import Required
+
+
 
 #for the picture upload
 from flask_wtf.file import FileField,FileAllowed
@@ -10,22 +12,34 @@ from wtforms.validators import DataRequired,Email
 from ..models import User
 
 
-
-class Session_typesForm(FlaskForm):
+class UpdateProfileForm(FlaskForm):
     '''
-    class to know the type of session
-
+    class to create a form to update user details
     '''
-    session=SelectField('Break or Work',choices=[('',''),('break','Break'),('work','Work')])
-    submit=SubmitField('Type')
+    email=StringField('Email',validators=[DataRequired(),Email()])
+    username=StringField('Username',validators=[DataRequired()])
+    picture=FileField('Update Profile Pic',validators=[FileAllowed(['jpg','png','jpeg'])])
+    submit=SubmitField('Update')
 
 
 
-class SessionForm(FlaskForm):
-    '''
-    class to know which sessions and time
 
-    '''
-    task=TextAreaField('Whats your current task',validators=[DataRequired()])
-    time=TextAreaField('how for hoe long?',validators=[DataRequired()])
-    submit=SubmitField('Session')
+
+# class Session_typesForm(FlaskForm):
+#     '''
+#     class to know the type of session
+
+#     '''
+#     session=SelectField('Break or Work',choices=[('',''),('break','Break'),('work','Work')])
+#     submit=SubmitField('Type')
+
+
+
+# class SessionForm(FlaskForm):
+#     '''
+#     class to know which sessions and time
+
+#     '''
+#     task=TextAreaField('Whats your current task',validators=[DataRequired()])
+#     time=TextAreaField('how for hoe long?',validators=[DataRequired()])
+#     submit=SubmitField('Session')
