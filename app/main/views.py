14600229a177d8e38c8f278d961b0adc57adf4ed
willyowi <wilsonowino1@gmail.com>
2019-08-20 13:55:55 +0300

    update views.py

--- app/main/views.py
+++ app/main/views.py
@@ -1,17 +1,39 @@
+
 from flask import render_template,request,redirect,url_for,abort,flash,request
 from . import main
 from flask_login import login_required,current_user
 from ..models import User,Session
 from ..import db
-from .forms import UpdateProfileForm
+from .forms import UpdateProfileForm,SessionForm
 from ..image_upload import add_profile_pic
+from ..timer import countdown
 
-@main.route('/')
+@main.route('/',methods=['GET','POST'])
 def index():
     '''
     view root function that returns index page and its data
     '''
-    return render_template('index.html',title='promodoro',user=current_user)
+    form=SessionForm()
+
+    if form.validate_on_submit():
+     
+
+      if form.session.data=='break':
+        session=Session(user_id=current_user.id,time=form.time.data,task=form.task.data,session_id=2)
+
+        session.save_session()
+        return redirect(url_for('main.profile',uname=current_user.username))
+
+      elif form.session.data=='work':
+         session=Session(user_id=current_user.id,time=form.time.data,task=form.task.data,session_id=1)
+
+         session.save_session()
+         return redirect(url_for('main.profile',uname=current_user.username))
+
+    flash('Session Saved','success')
+
+   
+    return render_template('index.html',title='promodoro',user=current_user,form=form)
 
 @main.route('/user/<uname>')
 def profile(uname):
@@ -20,12 +42,12 @@ def profile(uname):
     '''
 
     user=User.query.filter_by(username=uname).first()
-
+    sessions=Session.get_sessions()
 
     if user is None:
         abort(404)
 
-    return render_template('profile/user_profile.html',user=user)   
+    return render_template('profile/user_profile.html',user=user,sessions=sessions)   
 
     
 @main.route('/user/<uname>/profile_update',methods=['GET','POST'])
@@ -64,7 +86,4 @@ def profile_update(uname):
     
     #fetch the current profile image from the static folder and inject to the template    
     profile_image=url_for('static',filename='profile_pics/'+user.profile_pic_path)  
-    return render_template('profile/update_profile.html',form=form,profile_image=profile_image)  
-
-
-
+    return render_template('profile/update_profile.html',form=form,profile_image=profile_image) 
