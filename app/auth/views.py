03d23b03e48189cc837033e434cb13692587accc
willyowi <wilsonowino1@gmail.com>
2019-08-20 12:09:52 +0300

    update views.py

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,67 +1,45 @@
-from flask import render_template,request,redirect,url_for,abort,flash,request
-from . import main
-from flask_login import login_required,current_user
-from ..models import User,Session
-from ..import db
-from .forms import UpdateProfileForm
-from ..image_upload import add_profile_pic
-
-@main.route('/')
-def index():
-    '''
-    view root function that returns index page and its data
-    '''
-    return render_template('index.html',title='promodoro',user=current_user)
-
-@main.route('/user/<uname>')
-def profile(uname):
-    '''
-    view function to see a single user profile
-    '''
-
-    user=User.query.filter_by(username=uname).first()
+from flask import render_template,redirect,url_for,request,flash
+from . import auth
+from ..models import User
+from .forms import RegistrationForm,LoginForm
+from .. import db
+from flask_login import login_user,login_required,logout_user
 
 
-    if user is None:
-        abort(404)
-
-    return render_template('profile/user_profile.html',user=user)   
-
-    
-@main.route('/user/<uname>/profile_update',methods=['GET','POST'])
-@login_required
-def profile_update(uname):
-    '''
-    view function to updtae a user profile
-    '''
-    user=User.query.filter_by(username=uname).first()
-    if user is None:
-        abort(404)
-    
-    print(user)    
-
-    
-    form=UpdateProfileForm()
-
+@auth.route('/login',methods=['GET','POST'])
+def login():
+    form=LoginForm()
     if form.validate_on_submit():
-        
-        if form.picture.data:
-            pic=add_profile_pic(form.picture.data,user.username)
-            current_user.profile_image=pic
-        user.username=form.username.data
-        user.email=form.email.data
+        user=User.query.filter_by(email=form.email.data).first()
 
-        db.session.add(user) 
-        db.session.commit() 
+        if user is not None and user.verify_password(form.password.data):
+            login_user(user,form.remember.data)
 
-        return redirect(url_for('main.profile',uname=user.username)) 
+            return redirect(request.args.get('next') or url_for('main.index'))
 
-    elif request.method=='GET':
-        #user is not submitting anything
-        #set form field to the current users details
-        form.username.data=current_user.username
-        form.email.data=current_user.email
-    
-    #fetch the current profile image from the static folder and inject to the template    
-    profile_image=url_for('static',filename='profile_pics/'+user.profile_pic_path)  
-    return render_template('profile/update_profile.html',form=form,profile_image=profile_image) 
+        flash('Invalid Username or Password','danger')
+
+
+    title='Promodoro Login'
+    return render_template('auth/login.html',form=form,title=title) 
+
+
+
+@auth.route('/register',methods=['GET','POST'])
+def register():
+    form=RegistrationForm()
+    if form.validate_on_submit():
+        user=User(email=form.email.data,username=form.username.data,password=form.password.data)
+
+        user.save_user()
+        flash('Thanks for Joining Promodoro','success')
+        return redirect(url_for('main.index'))
+       
+    title='New Promodoro account'
+    return render_template('auth/register.html',form=form,title=title)    
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('main.index')) 
